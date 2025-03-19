54db931c1cc473cf1fdc86cca187154bd5a248cd
Nimus-oes <sseo.code@gmail.com>
2025-03-20 08:25:18 +0900

    Restructure directories

--- src/components/Analysis/Banner.jsx
+++
@@ -1,8 +0,0 @@
-export default function Banner(props) {
-  return (
-    <div className="banner-item">
-      <div className="banner-value">{props.value}</div>
-      <div className="banner-title">{props.title}</div>
-    </div>
-  );
-}

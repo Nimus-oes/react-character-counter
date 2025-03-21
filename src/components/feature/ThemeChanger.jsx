8314123ab55791ac5e80f4e7ed34e164502f06a3
Nimus-oes <sseo.code@gmail.com>
2025-03-21 09:33:06 +0900

    Move theme assets for cleaner structure

--- src/components/feature/ThemeChanger.jsx
+++ src/components/feature/ThemeChanger.jsx
@@ -1,35 +1,12 @@
 import { useState } from "react";
-import ImgButton from "../../components/ui/ImgButton.jsx";
-import logoLight from "../../assets/logo-light-theme.svg";
-import logoDark from "../../assets/logo-dark-theme.svg";
-import iconMoon from "../../assets/icon-moon.svg";
-import iconSun from "../../assets/icon-sun.svg";
-import bgDark from "../../assets/bg-dark-theme.png";
-import bgLight from "../../assets/bg-light-theme.png";
+import themeAsset from "../../assets/themeAsset";
 
 export default function ThemeChanger() {
   const [isDark, setIsDark] = useState(true);
   const changeTheme = () => {
     setIsDark(!isDark);
   };
-
-  const asset = {
-    dark: {
-      icon: iconSun,
-      iconalt: "An icon of sun representing a light mode",
-      logo: logoDark,
-      logoalt: "A logo image of character counter in dark mode",
-      bg: bgDark,
-    },
-    light: {
-      icon: iconMoon,
-      iconalt: "An icon of moon representing a dark mode",
-      logo: logoLight,
-      logoalt: "A logo image of character counter in light mode",
-      bg: bgLight,
-    },
-  };
-  const theme = isDark ? asset.dark : asset.light;
+  const theme = isDark ? themeAsset.dark : themeAsset.light;
 
   return (
     <>

abf3475fc08b5ed32dc1e73cb710350c730d8ff4
Nimus-oes <sseo.code@gmail.com>
2025-04-03 15:30:59 +0900

    Align density items

--- src/components/DensityList/Density.jsx
+++ src/components/DensityList/Density.jsx
@@ -3,14 +3,16 @@ import "./DensityList.css";
 export default function Density({ letter, count, ratio }) {
   return (
     <div className="density">
-      <label htmlFor={letter}>{letter}</label>
+      <label htmlFor={letter} className="letter">
+        {letter}
+      </label>
       <progress
         id={letter}
         value={ratio}
         max="100"
         className="progress"
       ></progress>
-      <div>{`${count} (${ratio}%)`}</div>
+      <div className="ratio">{`${count} (${ratio}%)`}</div>
     </div>
   );
 }
